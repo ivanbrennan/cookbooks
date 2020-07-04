7459c158ad98999831ab8d9dd016b8fa0134a976
ivanbrennan <ivan.brennan@gmail.com>
2020-07-04 11:25:04 -0400

    Response headers: add a header
    https://docs.servant.dev/en/stable/tutorial/Server.html#response-headers
    
    Example:
    
      $ curl -v localhost:8081
      ...
      < HTTP/1.1 200 OK
      < Transfer-Encoding: chunked
      < Date: Sat, 04 Jul 2020 16:35:29 GMT
      < Server: Warp/3.3.13
      < Content-Type: application/json;charset=utf-8
      < X-An-Int: 1797
      <
      * Connection #0 to host localhost left intact
      {"email":"ae@mc2.org","registration_date":"1905-12-01","age":136,"name":"Albert Einstein"}

--- servant/src/Cookbooks/Servant.hs
+++ servant/src/Cookbooks/Servant.hs
@@ -9,39 +9,48 @@ module Cookbooks.Servant
   )
 where
 
-import Control.Monad.IO.Class (liftIO)
 import Data.Aeson (ToJSON)
 import Data.Proxy (Proxy (Proxy))
+import Data.Time (Day, fromGregorian)
 import GHC.Generics (Generic)
 import Network.Wai.Handler.Warp (run)
-import Servant ((:>), Application, Get, JSON, Server, err404, errBody, serve, throwError)
-import System.Directory (doesFileExist)
+import Servant
+  ( Application,
+    Get,
+    Header,
+    Headers,
+    JSON,
+    Server,
+    addHeader,
+    serve,
+  )
 
-type IOAPI1 = "myfile.txt" :> Get '[JSON] FileContent
-
-newtype FileContent
-  = FileContent
-      {content :: String}
+data User
+  = User
+      { name :: String,
+        age :: Int,
+        email :: String,
+        registration_date :: Day
+      }
   deriving (Generic)
 
-instance ToJSON FileContent
+instance ToJSON User
 
-server6 :: Server IOAPI1
-server6 = do
-  exists <- liftIO (doesFileExist "myfile.txt")
-  if exists
-    then FileContent <$> liftIO (readFile "myfile.txt")
-    else throwError custom404Err
-  where
-    custom404Err = err404 {errBody = "myfile.txt just isn't there."}
+albert :: User
+albert = User "Albert Einstein" 136 "ae@mc2.org" (fromGregorian 1905 12 1)
 
-ioAPI :: Proxy IOAPI1
-ioAPI = Proxy
+type MyHandler = Get '[JSON] (Headers '[Header "X-An-Int" Int] User)
 
-app6 :: Application
-app6 = serve ioAPI server6
+myHandler :: Server MyHandler
+myHandler = pure (addHeader 1797 albert)
+
+headerAPI :: Proxy MyHandler
+headerAPI = Proxy
+
+app7 :: Application
+app7 = serve headerAPI myHandler
 
 runServer :: IO ()
 runServer = do
   putStrLn "Starting server"
-  run 8081 app6
+  run 8081 app7
