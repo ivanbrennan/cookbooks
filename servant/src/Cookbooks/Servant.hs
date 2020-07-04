54e2a5e921dc1a11ed1c1c3eb4e6e6712402e484
ivanbrennan <ivan.brennan@gmail.com>
2020-07-04 15:21:29 -0400

    Nested APIs (part 6)
    https://docs.servant.dev/en/stable/tutorial/Server.html#nested-apis
    
    Example:
    
      $ curl -v localhost:8081
      ...
      > GET / HTTP/1.1
      ...
      < HTTP/1.1 500 Internal Server Error
      ...
      Something went wrong
    
      $ curl -v localhost:8081 -H 'Content-Type: application/json' -d '{"email":"ae@mc2.org","registration_date":"1905-12-01","age":136,"name":"Foo"}'
      ...
      > POST / HTTP/1.1
      ...
      < HTTP/1.1 204 No Content
    
      $ curl -v localhost:8081/1
      ...
      > GET /1 HTTP/1.1
      ...
      < HTTP/1.1 500 Internal Server Error
      ...
      Something went wrong
    
      $ curl -v -X PUT localhost:8081/1 -H 'Content-Type: application/json' -d '{"email":"ae@mc2.org","registration_date":"1905-12-01","age":136,"name":"Foo"}'
      ...
      > PUT /1 HTTP/1.1
      ...
      < HTTP/1.1 204 No Content
    
      $ curl -v -X DELETE localhost:8081/1
      ...
      > DELETE /1 HTTP/1.1
      ...
      < HTTP/1.1 204 No Content

--- servant/src/Cookbooks/Servant.hs
+++ servant/src/Cookbooks/Servant.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators #-}
 
@@ -7,61 +8,72 @@ module Cookbooks.Servant
   )
 where
 
-import Data.Aeson (FromJSON, ToJSON, parseJSON, toJSON)
-import Data.ByteString (ByteString)
+import Data.Aeson (FromJSON, ToJSON)
 import Data.Proxy (Proxy (Proxy))
-import Data.Text.Encoding (decodeUtf8, encodeUtf8)
+import Data.Time (Day)
+import GHC.Generics (Generic)
 import Network.Wai.Handler.Warp (run)
 import Servant
   ( (:<|>) ((:<|>)),
     (:>),
     Application,
-    FromHttpApiData,
+    Capture,
+    DeleteNoContent,
     Get,
     Handler,
-    Header,
     JSON,
     NoContent (NoContent),
     PostNoContent,
+    PutNoContent,
     ReqBody,
     Server,
-    parseUrlPiece,
     serve,
   )
 
-type API3 =
-  Header "Authorization" Token
-    :> ( Get '[JSON] SecretData
-           :<|> ReqBody '[JSON] SecretData :> PostNoContent
-       )
+data User
+  = User
+      { name :: String,
+        age :: Int,
+        email :: String,
+        registration_date :: Day
+      }
+  deriving (Generic)
 
-newtype Token = Token ByteString
+instance FromJSON User
 
-instance FromHttpApiData Token where
-  parseUrlPiece = fmap (Token . encodeUtf8) . parseUrlPiece
+instance ToJSON User
 
-newtype SecretData = SecretData ByteString
+type UsersAPI =
+  Get '[JSON] [User]
+    :<|> ReqBody '[JSON] User :> PostNoContent
+    :<|> Capture "userid" Int
+      :> ( Get '[JSON] User
+             :<|> ReqBody '[JSON] User :> PutNoContent
+             :<|> DeleteNoContent
+         )
 
-instance ToJSON SecretData where
-  toJSON (SecretData x) = toJSON (decodeUtf8 x)
-
-instance FromJSON SecretData where
-  parseJSON = fmap (SecretData . encodeUtf8) . parseJSON
-
-server9 :: Server API3
-server9 Nothing = error "..."
-server9 (Just _) = getSecretData :<|> addSecretData
+usersServer :: Server UsersAPI
+usersServer = getUsers :<|> newUser :<|> userOperations
   where
-    getSecretData :: Handler SecretData
-    getSecretData = pure (SecretData "bar")
-    addSecretData :: SecretData -> Handler NoContent
-    addSecretData _secretData = pure NoContent
+    getUsers :: Handler [User]
+    getUsers = error "..."
+    newUser :: User -> Handler NoContent
+    newUser _user = pure NoContent
+    userOperations userid =
+      viewUser userid :<|> updateUser userid :<|> deleteUser userid
+      where
+        viewUser :: Int -> Handler User
+        viewUser = error "..."
+        updateUser :: Int -> User -> Handler NoContent
+        updateUser _userid _user = pure NoContent
+        deleteUser :: Int -> Handler NoContent
+        deleteUser _userid = pure NoContent
 
-staticAPI :: Proxy API3
-staticAPI = Proxy
+usersAPI :: Proxy UsersAPI
+usersAPI = Proxy
 
 app9 :: Application
-app9 = serve staticAPI server9
+app9 = serve usersAPI usersServer
 
 runServer :: IO ()
 runServer = do
