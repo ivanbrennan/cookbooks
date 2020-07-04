fa91d595e9e8887f809dd224a3cd0b85a9400f03
ivanbrennan <ivan.brennan@gmail.com>
2020-07-03 22:42:56 -0400

    Accept html: servant-blaze
    https://docs.servant.dev/en/stable/tutorial/Server.html#case-studies-servant-blaze-and-servant-lucid

--- servant/src/Cookbooks/Servant.hs
+++ servant/src/Cookbooks/Servant.hs
@@ -12,10 +12,10 @@ where
 import Data.Aeson (ToJSON)
 import Data.Proxy (Proxy (Proxy))
 import GHC.Generics (Generic)
-import Lucid (ToHtml, table_, td_, th_, toHtml, toHtmlRaw, tr_)
 import Network.Wai.Handler.Warp (run)
 import Servant ((:>), Application, Get, JSON, Server, serve)
-import Servant.HTML.Lucid (HTML)
+import Servant.HTML.Blaze (HTML)
+import Text.Blaze.Html5 (ToMarkup, table, td, th, toMarkup, tr)
 
 type PersonAPI = "persons" :> Get '[JSON, HTML] [Person]
 
@@ -28,22 +28,18 @@ data Person
 
 instance ToJSON Person
 
-instance ToHtml Person where
-  toHtml person =
-    tr_ $ do
-      td_ (toHtml $ firstName person)
-      td_ (toHtml $ lastName person)
+instance ToMarkup Person where
+  toMarkup person =
+    tr $ do
+      td (toMarkup $ firstName person)
+      td (toMarkup $ lastName person)
 
-  toHtmlRaw = toHtml
-
-instance ToHtml [Person] where
-  toHtml persons = table_ $ do
-    tr_ $ do
-      th_ "first name"
-      th_ "last name"
-    foldMap toHtml persons
-
-  toHtmlRaw = toHtml
+instance ToMarkup [Person] where
+  toMarkup persons = table $ do
+    tr $ do
+      th "first name"
+      th "last name"
+    foldMap toMarkup persons
 
 people :: [Person]
 people =
