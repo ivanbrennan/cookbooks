796004f3e9e611fab4c00fbf1bfd82ab965bf2de
ivanbrennan <ivan.brennan@gmail.com>
2020-07-04 14:51:53 -0400

    Nested APIs (part 5)
    https://docs.servant.dev/en/stable/tutorial/Server.html#nested-apis
    
    Example:
    
      $ curl localhost:8081
      Something went wrong
    
      $ curl localhost:8081 -H 'Authorization: foo'
      "bar"

--- servant/src/Cookbooks/Servant.hs
+++ servant/src/Cookbooks/Servant.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators #-}
 
@@ -8,53 +7,57 @@ module Cookbooks.Servant
   )
 where
 
-import Data.Aeson (FromJSON, ToJSON)
+import Data.Aeson (FromJSON, ToJSON, parseJSON, toJSON)
+import Data.ByteString (ByteString)
 import Data.Proxy (Proxy (Proxy))
-import Data.Time (Day)
-import GHC.Generics (Generic)
+import Data.Text.Encoding (decodeUtf8, encodeUtf8)
 import Network.Wai.Handler.Warp (run)
 import Servant
   ( (:<|>) ((:<|>)),
     (:>),
     Application,
+    FromHttpApiData,
     Get,
     Handler,
+    Header,
     JSON,
     NoContent (NoContent),
     PostNoContent,
     ReqBody,
     Server,
+    parseUrlPiece,
     serve,
   )
 
-data User
-  = User
-      { name :: String,
-        age :: Int,
-        email :: String,
-        registration_date :: Day
-      }
-  deriving (Generic)
-
-instance FromJSON User
-
-instance ToJSON User
-
-type API2 =
-  ReqBody '[JSON] User
-    :> ( Get '[JSON] User
-           :<|> PostNoContent
+type API3 =
+  Header "Authorization" Token
+    :> ( Get '[JSON] SecretData
+           :<|> ReqBody '[JSON] SecretData :> PostNoContent
        )
 
-server9 :: Server API2
-server9 user = getUser :<|> registerUser
-  where
-    getUser :: Handler User
-    getUser = pure user
-    registerUser :: Handler NoContent
-    registerUser = pure NoContent
+newtype Token = Token ByteString
 
-staticAPI :: Proxy API2
+instance FromHttpApiData Token where
+  parseUrlPiece = fmap (Token . encodeUtf8) . parseUrlPiece
+
+newtype SecretData = SecretData ByteString
+
+instance ToJSON SecretData where
+  toJSON (SecretData x) = toJSON (decodeUtf8 x)
+
+instance FromJSON SecretData where
+  parseJSON = fmap (SecretData . encodeUtf8) . parseJSON
+
+server9 :: Server API3
+server9 Nothing = error "..."
+server9 (Just _) = getSecretData :<|> addSecretData
+  where
+    getSecretData :: Handler SecretData
+    getSecretData = pure (SecretData "bar")
+    addSecretData :: SecretData -> Handler NoContent
+    addSecretData _secretData = pure NoContent
+
+staticAPI :: Proxy API3
 staticAPI = Proxy
 
 app9 :: Application
