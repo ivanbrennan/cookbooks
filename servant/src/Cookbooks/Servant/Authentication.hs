fabc55fa41974dc45c917f1b268cab80f6784e72
ivanbrennan <ivan.brennan@gmail.com>
2020-07-08 10:10:08 -0400

    Generalized authentication
    https://docs.servant.dev/en/stable/tutorial/Authentication.html#generalized-authentication
    
    Example:
    
      $ curl -i localhost:8080/public
    
      HTTP/1.1 200 OK
      Transfer-Encoding: chunked
      Date: Wed, 08 Jul 2020 14:08:19 GMT
      Server: Warp/3.3.13
      Content-Type: application/json;charset=utf-8
    
      [{"somedata":"this is a public piece of data"}]
    
      $ curl -i localhost:8080/private
    
      HTTP/1.1 401 Unauthorized
      Transfer-Encoding: chunked
      Date: Wed, 08 Jul 2020 14:08:26 GMT
      Server: Warp/3.3.13
    
      Missing cookie header
    
      $ curl -i localhost:8080/private -H "Cookie: servant-auth-cookie=key3"
    
      HTTP/1.1 200 OK
      Transfer-Encoding: chunked
      Date: Wed, 08 Jul 2020 14:09:32 GMT
      Server: Warp/3.3.13
      Content-Type: application/json;charset=utf-8
    
      {"ssshhh":"this is a secret: Ghédalia Tazartès"}
    
      $ curl -i localhost:8080/private -H "Cookie: servant-auth-cookie=bad-key"
    
      HTTP/1.1 403 Forbidden
      Transfer-Encoding: chunked
      Date: Wed, 08 Jul 2020 14:09:42 GMT
      Server: Warp/3.3.13
    
      Invalid Cookie

--- servant/src/Cookbooks/Servant/Authentication.hs
+++ servant/src/Cookbooks/Servant/Authentication.hs
@@ -1,32 +1,47 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Cookbooks.Servant.Authentication
   ( basicAuthMain,
+    genAuthMain,
   )
 where
 
 import Data.Aeson (ToJSON)
+import Data.ByteString (ByteString)
+import Data.Map (Map)
+import qualified Data.Map as Map
 import Data.Proxy (Proxy (Proxy))
 import Data.Text (Text)
 import GHC.Generics (Generic)
+import Network.Wai (Request, requestHeaders)
 import Network.Wai.Handler.Warp (run)
 import Servant
   ( (:<|>) ((:<|>)),
     (:>),
+    AuthProtect,
     BasicAuth,
     BasicAuthCheck (BasicAuthCheck),
     BasicAuthData (BasicAuthData),
     BasicAuthResult (Authorized, Unauthorized),
     Context ((:.), EmptyContext),
     Get,
+    Handler,
     JSON,
     Server,
+    err401,
+    err403,
+    errBody,
     serveWithContext,
+    throwError,
   )
+import Servant.Server.Experimental.Auth (AuthHandler, AuthServerData, mkAuthHandler)
+import Web.Cookie (parseCookies)
 
 newtype PrivateData = PrivateData {ssshhh :: Text}
   deriving (Generic)
@@ -77,3 +92,49 @@ basicAuthMain =
         basicAuthServerContext
         basicAuthServer
     )
+
+newtype Account = Account Text
+
+database :: Map ByteString Account
+database =
+  Map.fromList
+    [ ("key1", Account "Anne Briggs"),
+      ("key2", Account "Bruce Cockburn"),
+      ("key3", Account "Ghédalia Tazartès")
+    ]
+
+lookupAccount :: ByteString -> Handler Account
+lookupAccount key = case Map.lookup key database of
+  Nothing -> throwError (err403 {errBody = "Invalid Cookie"})
+  Just usr -> pure usr
+
+authHandler :: AuthHandler Request Account
+authHandler = mkAuthHandler handler
+  where
+    maybeToEither e = maybe (Left e) Right
+    throw401 msg = throwError $ err401 {errBody = msg}
+    handler req = either throw401 lookupAccount $ do
+      cookie <- maybeToEither "Missing cookie header" $ lookup "cookie" $ requestHeaders req
+      maybeToEither "Missing token in cookie" $ lookup "servant-auth-cookie" $ parseCookies cookie
+
+type AuthGenAPI =
+  "private" :> AuthProtect "cookie-auth" :> PrivateAPI
+    :<|> "public" :> PublicAPI
+
+genAuthAPI :: Proxy AuthGenAPI
+genAuthAPI = Proxy
+
+type instance AuthServerData (AuthProtect "cookie-auth") = Account
+
+genAuthServerContext :: Context (AuthHandler Request Account ': '[])
+genAuthServerContext = authHandler :. EmptyContext
+
+genAuthServer :: Server AuthGenAPI
+genAuthServer =
+  let privateDataFunc (Account name) =
+        pure (PrivateData ("this is a secret: " <> name))
+      publicData = pure [PublicData "this is a public piece of data"]
+   in privateDataFunc :<|> publicData
+
+genAuthMain :: IO ()
+genAuthMain = run 8080 (serveWithContext genAuthAPI genAuthServerContext genAuthServer)
